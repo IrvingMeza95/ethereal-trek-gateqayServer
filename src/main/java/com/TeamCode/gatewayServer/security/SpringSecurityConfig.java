3a23a6bee4668f2f3e331f099c21c23932a4a97a
Irving Meza <irvingmeza95@gmail.com>
2024-03-09 19:42:53 -0600

    se agregaron la configuracion de cors.

--- src/main/java/com/TeamCode/gatewayServer/security/SpringSecurityConfig.java
+++ src/main/java/com/TeamCode/gatewayServer/security/SpringSecurityConfig.java
@@ -1,18 +1,30 @@
 package com.TeamCode.gatewayServer.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableWebFluxSecurity
 public class SpringSecurityConfig {
 	
 	@Autowired
 	private JwtAuthenticationFilter authenticationFilter;
+	@Value("${config.security.cors.origins}")
+	private List<String> origin;
+	@Value("${config.security.cors.methods}")
+	private List<String> methods;
+	@Value("${config.security.cors.headers}")
+	private List<String> headers;
 
 	@Bean
 	public SecurityWebFilterChain configure(ServerHttpSecurity http) {
@@ -25,9 +37,23 @@ public class SpringSecurityConfig {
 						"/api/services/**").hasAnyRole("ADMIN", "USER")
 				.pathMatchers("/api/usuarios/**", "/api/usuarios/roles/**", "/api/services/**").hasRole("ADMIN")
 				.anyExchange().authenticated()
+				.and().cors().configurationSource(corsConfigurationSource())
 				.and().addFilterAt(authenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
 				.csrf().disable()
 				.build();
 	}
-	
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(origin);
+		corsConfig.setAllowedMethods(methods);
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(headers);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+
 }
